5a91ff35e245063d1325d814d20b07f278ed4fce
Niklas Schikora <schikora@dasachtegebot.de>
2021-11-10 23:48:23 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import ConfigPanel from "./components/config/ConfigPanel";
+import Footer from "./components/ui/Footer";
 import TopBar from "./components/ui/TopBar";
 
 function App() {
   return (
-    <div className="wrapper">
-      <TopBar />
-      <div class="container mt-5">
-        <h1 class="is-size-1">Electric Car Configurator</h1>
+    <div className="content">
+      <div className="wrapper">
+        <TopBar />
+        <div class="container mt-5">
+          <h1 class="is-size-1">Electric Car Configurator</h1>
+        </div>
+        <ConfigPanel />
       </div>
-      <ConfigPanel />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/ui/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="footer has-background-white">
+      <div className="content has-text-centered">
+        <hr />
+        <p>
+          <strong>Demo Configurator</strong> by Niklas Schikora. This page uses
+          images by <a href="https://www.tesla.com/">Tesla Motors</a>. All
+          images are courtesy of{" "}
+          <a href="https://www.tesla.com/">Tesla Motors</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
